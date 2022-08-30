4574ef251a0a1e9b29befb6c3d1d8372a82cc4d5
Ramona Schwering <29896429+leichteckig@users.noreply.github.com>
2022-08-30 22:16:42 +0200

    First testing notes

--- cypress/e2e/find-my-session.cy.js
+++ cypress/e2e/find-my-session.cy.js
@@ -7,21 +7,32 @@ describe('Schedule of Web Summer Camp', () => {
       url: 'https://forms.hsforms.com/embed/v3/form/**/**/*'
     }).as('formsCall');
 
-    // Open menu
+    // #1: Direct step
     cy.visit('/');
+    // #2: Polishing - Waiting for UI
     cy.contains('.title', 'Web Summer Camp 2022').should('be.visible');
     cy.get('.closed').should('be.visible');
+
+    // #1: Direct step
     cy.get('.closed').click();
 
-    // Find session in schedule
+    // #1: Direct step
     cy.contains('.main-navigation a', 'Program').click();
+    // #2: Polishing - Waiting for UI
     cy.contains('.table.day-two tr', 'By Ramona Schwering').scrollIntoView();
 
-    // Open and verify schedule
+    // #1: Direct step
     cy.get('a[href="/end-to-end-testing-as-it-should-be-an-introduction-to-cypress"]').click();
+
+    // #1: Direct step (already better waiting, so use as transition)
     cy.contains('p', 'End-To-End Testing').should('be.visible');
+    // #2: Polishing - Waiting for UI
     cy.get('.image').scrollIntoView();
+
+    // #3: Polishing - API waiting
     cy.wait('@formsCall').its('response.statusCode').should('eq', 200);
+
+    // #1: Direct step
     cy.contains('.name', 'Ramona Schwering').should('be.visible');
   });
 });
